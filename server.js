ee375ec1e4a04ce23a3b4c11a6ce260fa7abc317
Kevin Stubblefield <kevins12.92@gmail.com>
2017-04-09 23:36:09 -0600

    Convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
     var query = req.query;
     var where = {};
 
+    where.userId = req.user.get('id');
+
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
         where.completed = true;
     } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
@@ -46,7 +48,12 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
     var todoId = parseInt(req.params.id, 10);
 
-    db.todo.findById(todoId).then(function (todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function (todo) {
         if (todo) {
             res.json(todo.toJSON());
         } else {
@@ -78,7 +85,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 
     db.todo.destroy({
         where: {
-            id: todoId
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function (rowsDeleted) {
         if (rowsDeleted === 0) {
@@ -107,7 +115,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
         attributes.description = body.description;
     }
 
-    db.todo.findById(todoId).then(function (todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function (todo) {
         if (todo) {
             todo.update(attributes).then(function (todo) {
                 res.json(todo.toJSON());
@@ -149,7 +162,7 @@ app.post('/users/login', function (req, res) {
     });
 });
 
-db.sequelize.sync({force: true}).then(function () {
+db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log('Express listening on port ' + PORT + '!');
     });
